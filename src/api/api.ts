a9f133dede702f3ead289d9307b05b35539ee7b1
yardencohavi <cohaviyarden@gmail.com>
2024-08-17 22:24:16 +0300

    create my-json-server

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,7 +2,7 @@ import axios, { AxiosResponse } from "axios";
 import { BudgetForm } from "../types";
 
 const API_URL = "http://localhost:3001";
-
+const DB_URL = "https://my-json-server.typicode.com/yardencohavi/backend-db";
 export interface QuestionDTO {
   id: number;
   question: string;
@@ -13,7 +13,7 @@ export interface QuestionDTO {
 export const getBudgetFormData = async (): Promise<BudgetForm> => {
   try {
     const response: AxiosResponse<BudgetForm> = await axios.get(
-      `${API_URL}/budget`
+      `${DB_URL}/budget`
     );
 
     return response.data;
