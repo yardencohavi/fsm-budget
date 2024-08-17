3fe5cc8c9c9dc958893f584a61ba5d4f1ace8493
yardencohavi <cohaviyarden@gmail.com>
2024-08-17 19:02:55 +0300

    Updates

--- src/components/BudgetForm/styles.ts
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const Container = styled.div`
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-  padding: 30px 10px;
-  gap: 50px;
-`;
